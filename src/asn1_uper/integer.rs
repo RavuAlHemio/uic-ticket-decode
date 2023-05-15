558f416fdaa748bce4d7e32fd204cce7e628762d
Ondřej Hošek <ondra.hosek@gmail.com>
2023-05-15 23:35:01 +0200

    decoding progress

--- src/asn1_uper/integer.rs
+++ src/asn1_uper/integer.rs
@@ -2,7 +2,10 @@
 
 
 use std::borrow::Cow;
-use std::ops::{Add, BitAnd, BitOr, BitXor, Div, Mul, Neg, Rem, Shl, Shr, Sub};
+use std::ops::{
+    Add, AddAssign, BitAnd, BitAndAssign, BitOr, BitOrAssign, BitXor, BitXorAssign, Div, DivAssign,
+    Mul, MulAssign, Neg, Rem, RemAssign, Shl, ShlAssign, Shr, ShrAssign, Sub, SubAssign,
+};
 
 use num_bigint::BigInt;
 use once_cell::sync::Lazy;
@@ -15,6 +18,7 @@ static SHORT_MAX: Lazy<BigInt> = Lazy::new(|| BigInt::from(ShortInt::MAX));
 
 
 /// An ASN.1 arbitrary-size integer.
+#[derive(Clone, Debug, Eq, Hash, Ord, PartialEq, PartialOrd)]
 pub struct Integer {
     inner: IntegerInner,
 }
@@ -36,6 +40,22 @@ impl Integer {
                 inner: IntegerInner::Long(value),
             }
         }
+    }
+
+    /// Creates an ASN.1 integer from the given slice of bits.
+    ///
+    /// Bits are assumed to be ordered from most to least significant. Note that this can only
+    /// create unsigned integers.
+    pub fn from_bits(bits: &[bool]) -> Self {
+        let one = Self::from_short(1);
+        let mut value = Self::from_short(0);
+        for &bit in bits {
+            value <<= 1;
+            if bit {
+                value += &one;
+            }
+        }
+        value
     }
 
     /// Returns the inner integer as a BigInt value.
@@ -118,17 +138,97 @@ macro_rules! implement_bin_op {
             }
         }
     };
+    (@assign_infallible, $assign_trait:ident, $assign_func:ident, $binop_trait:ident, $binop_func:ident) => {
+        impl $assign_trait<&Integer> for Integer {
+            fn $assign_func(&mut self, rhs: &Self) {
+                match (&mut self.inner, &rhs.inner) {
+                    (IntegerInner::Short(l), IntegerInner::Short(r)) => {
+                        // stay short
+                        $assign_trait::$assign_func(l, *r)
+                    },
+                    // otherwise, work with BigInt and reduce if possible
+                    (IntegerInner::Short(l), IntegerInner::Long(r)) => {
+                        *self = Integer::from_long($binop_trait::$binop_func(BigInt::from(*l), r));
+                    },
+                    (IntegerInner::Long(l), IntegerInner::Short(r)) => {
+                        *self = Integer::from_long($binop_trait::$binop_func(&*l, BigInt::from(*r)));
+                    },
+                    (IntegerInner::Long(l), IntegerInner::Long(r)) => {
+                        *self = Integer::from_long($binop_trait::$binop_func(&*l, r));
+                    },
+                }
+            }
+        }
+    };
+    (@assign_checked, $assign_trait:ident, $assign_func:ident, $binop_trait:ident, $binop_func:ident, $checked_op_func:ident) => {
+        impl $assign_trait<&Integer> for Integer {
+            fn $assign_func(&mut self, rhs: &Self) {
+                match (&mut self.inner, &rhs.inner) {
+                    (IntegerInner::Short(l), IntegerInner::Short(r)) => {
+                        // stay short if it fits
+                        match l.$checked_op_func(*r) {
+                            Some(sum) => {
+                                *l = sum;
+                            },
+                            None => {
+                                *self = Integer::from_long($binop_trait::$binop_func(BigInt::from(*l), BigInt::from(*r)));
+                            },
+                        }
+                    },
+                    // otherwise, work with BigInt and reduce if possible
+                    (IntegerInner::Short(l), IntegerInner::Long(r)) => {
+                        *self = Integer::from_long($binop_trait::$binop_func(BigInt::from(*l), r));
+                    },
+                    (IntegerInner::Long(l), IntegerInner::Short(r)) => {
+                        *self = Integer::from_long($binop_trait::$binop_func(&*l, BigInt::from(*r)));
+                    },
+                    (IntegerInner::Long(l), IntegerInner::Long(r)) => {
+                        *self = Integer::from_long($binop_trait::$binop_func(&*l, r));
+                    },
+                }
+            }
+        }
+    };
+    (@assign_checked_rhs, $assign_trait:ident, $assign_func:ident, $binop_trait:ident, $binop_func:ident, $checked_op_func:ident, $rhs_type:ty) => {
+        impl $assign_trait<$rhs_type> for Integer {
+            fn $assign_func(&mut self, rhs: $rhs_type) {
+                match &mut self.inner {
+                    IntegerInner::Short(l) => {
+                        // stay short if it fits
+                        match l.$checked_op_func(rhs) {
+                            Some(sum) => *l = sum,
+                            None => *self = Integer::from_long($binop_trait::$binop_func(BigInt::from(*l), rhs)),
+                        }
+                    },
+                    IntegerInner::Long(l) => {
+                        // work with BigInt and reduce if possible
+                        *self = Integer::from_long($binop_trait::$binop_func(&*l, rhs));
+                    },
+                }
+            }
+        }
+    };
 }
 implement_bin_op!(@checked, Add, add, checked_add);
+implement_bin_op!(@assign_checked, AddAssign, add_assign, Add, add, checked_add);
 implement_bin_op!(@infallible, BitAnd, bitand);
+implement_bin_op!(@assign_infallible, BitAndAssign, bitand_assign, BitAnd, bitand);
 implement_bin_op!(@infallible, BitOr, bitor);
+implement_bin_op!(@assign_infallible, BitOrAssign, bitor_assign, BitOr, bitor);
 implement_bin_op!(@infallible, BitXor, bitxor);
+implement_bin_op!(@assign_infallible, BitXorAssign, bitxor_assign, BitXor, bitxor);
 implement_bin_op!(@checked, Div, div, checked_div);
+implement_bin_op!(@assign_checked, DivAssign, div_assign, Div, div, checked_div);
 implement_bin_op!(@checked, Mul, mul, checked_mul);
+implement_bin_op!(@assign_checked, MulAssign, mul_assign, Mul, mul, checked_mul);
 implement_bin_op!(@checked, Rem, rem, checked_rem);
+implement_bin_op!(@assign_checked, RemAssign, rem_assign, Rem, rem, checked_rem);
 implement_bin_op!(@checked_rhs, Shl, shl, checked_shl, u32);
+implement_bin_op!(@assign_checked_rhs, ShlAssign, shl_assign, Shl, shl, checked_shl, u32);
 implement_bin_op!(@checked_rhs, Shr, shr, checked_shr, u32);
+implement_bin_op!(@assign_checked_rhs, ShrAssign, shr_assign, Shr, shr, checked_shr, u32);
 implement_bin_op!(@checked, Sub, sub, checked_sub);
+implement_bin_op!(@assign_checked, SubAssign, sub_assign, Sub, sub, checked_sub);
 impl Neg for Integer {
     type Output = Integer;
 
--- src/asn1_uper/mod.rs
+++ src/asn1_uper/mod.rs
@@ -7,6 +7,120 @@
 mod integer;
 
 
-pub fn decode_constrained_int() {
+use nom::error::{Error as NomError, ErrorKind as NomErrorKind};
 
+use self::integer::Integer;
+
+
+pub type ParseResult<'a, T> = nom::IResult<&'a [bool], T>;
+
+
+/// A description of what range a whole number may occupy.
+#[derive(Clone, Debug, Default, Hash, Eq, Ord, PartialEq, PartialOrd)]
+pub enum WholeNumberConstraint {
+    /// A whole number that may take any value.
+    ///
+    /// Defined in X.691 § 3.7.27.
+    #[default] Unconstrained,
+
+    /// A whole number which may take any value greater than or equal to a minimum value.
+    ///
+    /// Defined in X.691 § 3.7.24.
+    SemiConstrained { min: Integer },
+
+    /// A whole number which is greater than or equal to a minimum value and less than or equal to a
+    /// maximum value.
+    ///
+    /// Defined in X.691 § 3.7.7.
+    Constrained { min: Integer, max: Integer },
+}
+
+
+/// Calculates the minimum number of bits required to encode a given number of unique values.
+fn bits_required_for_unique_values(n: &Integer) -> usize {
+    let zero = Integer::from_short(0);
+
+    assert!(n > &zero);
+
+    let mut value = n - &Integer::from_short(1);
+    let mut count = 0;
+    while value > zero {
+        value >>= 1;
+        count += 1;
+    }
+    count
+}
+
+
+fn get_eof<'a, T>(bits: &'a [bool]) -> ParseResult<'a, T> {
+    return Err(nom::Err::Error(NomError::new(bits, NomErrorKind::Eof)));
+}
+
+
+/// Decodes a constrained whole number.
+///
+/// The encoding process is described in X.691 § 11.5.
+fn decode_constrained_int<'a>(bits: &'a [bool], min: &Integer, max: &Integer) -> ParseResult<'a, Integer> {
+    assert!(min < max);
+    let range = &(max - min) + &Integer::from_short(1);
+    if &range == &Integer::from_short(1) {
+        // an integer that can only occupy one value is encoded into zero bits (X.691 § 11.5.4);
+        // just return min
+        return Ok((bits, min.clone()));
+    }
+
+    // what is encoded is not `n` but `n - min` (as a non-negative binary integer; § 11.5.6)
+    let bit_count = bits_required_for_unique_values(&range);
+
+    // take as many bits
+    if bits.len() < bit_count {
+        return get_eof(bits);
+    }
+    let (my_bits, rest) = bits.split_at(bit_count);
+    let my_value = Integer::from_bits(my_bits);
+
+    Ok((rest, my_value))
+}
+
+
+/// Decodes a normally-small non-negative integer.
+///
+/// The encoding process is described in X.691 § 11.6.
+fn decode_normally_small_nonneg_int<'a>(bits: &'a [bool]) -> ParseResult<'a, Integer> {
+    if bits.len() < 1 {
+        return get_eof(bits);
+    }
+    let (is_large, rest) = bits.split_at(1);
+    if is_large[0] {
+        // X.691 § 11.6.2 forwards us to § 11.7 (semi-constrained integer)
+        // with § 11.9 to govern the length
+        todo!();
+    } else {
+        // take six bits
+        if rest.len() < 6 {
+            return get_eof(bits);
+        }
+        let (my_bits, rest) = bits.split_at(6);
+        let my_value = Integer::from_bits(my_bits);
+        Ok((rest, my_value))
+    }
+}
+
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_bits_required() {
+        assert_eq!(bits_required_for_unique_values(&Integer::from_short(1)), 0);
+        assert_eq!(bits_required_for_unique_values(&Integer::from_short(2)), 1);
+        assert_eq!(bits_required_for_unique_values(&Integer::from_short(3)), 2);
+        assert_eq!(bits_required_for_unique_values(&Integer::from_short(4)), 2);
+        assert_eq!(bits_required_for_unique_values(&Integer::from_short(5)), 3);
+        assert_eq!(bits_required_for_unique_values(&Integer::from_short(6)), 3);
+        assert_eq!(bits_required_for_unique_values(&Integer::from_short(7)), 3);
+        assert_eq!(bits_required_for_unique_values(&Integer::from_short(8)), 3);
+        assert_eq!(bits_required_for_unique_values(&Integer::from_short(9)), 4);
+    }
 }
